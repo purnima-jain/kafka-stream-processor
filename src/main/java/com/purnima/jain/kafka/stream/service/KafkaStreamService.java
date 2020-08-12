428b4eb4da40f7afc24a78fc38cc312a2a697b21
Purnima Jain <jain_purnima@hotmail.com>
2020-08-12 19:40:48 +0530

    Removed log statements and add Stream print statements.

--- src/main/java/com/purnima/jain/kafka/stream/service/KafkaStreamService.java
+++ src/main/java/com/purnima/jain/kafka/stream/service/KafkaStreamService.java
@@ -6,8 +6,6 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.support.serializer.JsonSerde;
@@ -18,8 +16,6 @@ import com.purnima.jain.kafka.rest.dto.UserDto;
 @Component
 public class KafkaStreamService {
 	
-	private static final Logger logger = LoggerFactory.getLogger(KafkaStreamService.class);
-	
 	@Value("#{kafkaConfig.getStreamInputTopicName()}")
 	private String streamInputTopicName;
 	
@@ -28,15 +24,17 @@ public class KafkaStreamService {
 	
 	@Bean
 	public KStream<String, UserDto> kStream(StreamsBuilder kStreamBuilder) {
-		logger.info("Entering KafkaStreamsConfig.kStream()............");
 		KStream<String, UserDto> stream = kStreamBuilder.stream(streamInputTopicName, Consumed.with(Serdes.String(), new JsonSerde<>(UserDto.class)));
     	
-		stream.print(Printed.toSysOut());
-		stream.mapValues(userDto -> userDto.convertFirstNameToUpperCase())
-				.to(streamOutputTopicName, Produced.with(Serdes.String(), new JsonSerde<>(UserDto.class)));
+		stream.print(Printed.<String, UserDto>toSysOut().withLabel("KafkaStreamProcessingApplication :: Before :: "));
+		
+		KStream<String, UserDto> upperCasedStream = stream.mapValues(userDto -> userDto.convertFirstNameToUpperCase());
+		
+		upperCasedStream.to(streamOutputTopicName, Produced.with(Serdes.String(), new JsonSerde<>(UserDto.class)));
+		
+		upperCasedStream.print(Printed.<String, UserDto>toSysOut().withLabel("KafkaStreamProcessingApplication :: After :: "));
         
-		logger.info("Leaving KafkaStreamsConfig.kStream()............");
-        return stream;
+        return upperCasedStream;
 	}
 
 }
